5a79df4dab1ffafd3e2dfdc74edaa026f8f397b5
listenyoon <84116086+listenyoon@users.noreply.github.com>
2021-07-21 17:50:14 +0900

    add Menu Button
    개인설정으로 링크 연결
    내부 css 정리함

--- coin/src/Pages/Main/Main.js
+++ coin/src/Pages/Main/Main.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import BoardContent from '../../Components/BoardContent';
-import BoardInfo from '../../Components/BoardInfo';
-function Main(){
-    return(
-        <>
-            <BoardContent>
-                <BoardInfo>
-                    <h2>Board</h2>
-                </BoardInfo>
-            </BoardContent>
-        </>
+import React from "react";
+import BoardContent from "../../Components/BoardContent";
+import BoardInfo from "../../Components/BoardInfo";
+import Menu from "../../Components/Menu";
+function Main() {
+  return (
+    <>
+      <BoardContent>
+        <BoardInfo>
+          <h2>Board</h2>
+        </BoardInfo>
+      </BoardContent>
 
-
-
-    );
+      <Menu />
+    </>
+  );
 }
 
 export default Main;
