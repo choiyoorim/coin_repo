7e15f6a8ba5778eed42530a479ff92f7ba4b6ec4
listenyoon <84116086+listenyoon@users.noreply.github.com>
2021-07-23 17:03:31 +0900

    add QuitPage
    myPage 탈퇴버튼->quitPage연결
    quit 성공 이후 homePage로 연결

--- coin/src/App.js
+++ coin/src/App.js
@@ -1,33 +1,30 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
-import{
-  BrowserRouter,
-  Switch,
-  Route,
-  Router,
-} from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Router } from "react-router-dom";
 
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import Main from './Pages/Main/Main';
-import MyPage from './Pages/MyPage/MyPage';
-import SignUp from './Pages/SignUp/SignUp';
-import SendMail from './Pages/Sendmail/Sendmail';
-import FindPw from './Pages/FindPw/FindPw';
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Main from "./Pages/Main/Main";
+import MyPage from "./Pages/MyPage/MyPage";
+import SignUp from "./Pages/SignUp/SignUp";
+import SendMail from "./Pages/Sendmail/Sendmail";
+import FindPw from "./Pages/FindPw/FindPw";
+import Quit from "./Pages/Quit/Quit";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-          <Route exact path="/" component={Home}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/main" component={Main}/>
-          <Route path="/mypage" component={MyPage}/>
-          <Route path="/signup" component={SignUp}/>
-          <Route path="/sendmail" component={SendMail}/>
-          <Route path="/findpw" component={FindPw}/>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/main" component={Main} />
+        <Route path="/mypage" component={MyPage} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/sendmail" component={SendMail} />
+        <Route path="/findpw" component={FindPw} />
+        <Route path="/quit" component={Quit} />
+      </BrowserRouter>
     </>
   );
 }
