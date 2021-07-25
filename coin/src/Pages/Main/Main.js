08382ac387b73f548aa119865c9e0a228ce11c16
plum-king <jeesoo0908@gmail.com>
2021-07-25 13:54:20 +0900

    mod login and css

--- coin/src/Pages/Main/Main.js
+++ coin/src/Pages/Main/Main.js
@@ -3,13 +3,13 @@ import BoardContent from '../../Components/BoardContent';
 import BoardInfo from '../../Components/BoardInfo';
 function Main(){
     return(
-        <>
+        <Menu>
             <BoardContent>
                 <BoardInfo>
                     <h2>Board</h2>
                 </BoardInfo>
             </BoardContent>
-        </>
+        </Menu>
 
 
 
