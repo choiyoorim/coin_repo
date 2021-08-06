5a2c876fd81637f91d531a9bedbad4f956903304
plum-king <77599304+plum-king@users.noreply.github.com>
2021-08-06 15:26:05 +0900

    mod signup

--- coin/src/Pages/SignUp/SignUp.js
+++ coin/src/Pages/SignUp/SignUp.js
@@ -1,19 +1,17 @@
-import React,{useEffect} from "react";
+import React from "react";
 import LoginHeader from "../../Components/LoginHeader";
 import SignUpContent from "../../Components/SignUpContent";
 import './SignUp.css'
-import axios from "axios";
-import { withRouter } from "react-router";
 
-function SignUp(){
-  
+class SignUp extends React.Component {
+  render() {
     return (
       <div className="wrap-login">
         <LoginHeader />
         <SignUpContent />
       </div>
     );
+  }
 }
-
 
 export default SignUp;
