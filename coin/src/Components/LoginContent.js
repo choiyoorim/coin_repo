c105078589d5d2f65d63f288f42236597603a8e9
plum-king <77599304+plum-king@users.noreply.github.com>
2021-08-06 15:31:06 +0900

    mod loginContent

--- coin/src/Components/LoginContent.js
+++ coin/src/Components/LoginContent.js
@@ -1,72 +1,25 @@
-import React, { Component,useState,useEffect } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
-import { loginUser } from "../action/userAction";
-import {useDispatch} from 'react-redux';
-import 'react-router-dom';
-import { withRouter } from "react-router";
-
-function LoginContent({history}) {
-  const [Id,setId] = useState("")
-  const [Password,setPassword] = useState("")
-  const dispatch = useDispatch();
-
-  const onIdHandler = (event) =>{
-    setId(event.currentTarget.value)
+class LoginContent extends Component {
+  render() {
+    return (
+      <div className="content">
+        <form class = "loginContent">
+          <input className="input_id" type="id" placeholder="id" />
+          <br />
+          <input className="input_pwd" type="password" placeholder="password" />
+          <br />
+          <button className="btn" type="submit">Login</button>
+          <br />
+          <a>카카오톡으로 로그인하기</a>
+          <br />
+          <a target="_blank" id="pw-inquiry" href="#">
+          <Link to="signup">회원가입</Link> | <Link to="findpw">비밀번호 찾기</Link>
+          </a>
+        </form>
+      </div>
+    );
   }
-
-  const onPasswordHandler = (event) =>{
-    setPassword(event.currentTarget.value)
-  }
-
-  const onSubmitHandler = (event) =>{
-    event.preventDefault();
-
-    let body = {
-      id : Id,
-      password : Password
-    };
-
-    dispatch(loginUser(body))
-      .then((res) => {
-        if (res.payload.loginSuccess) {
-          alert("login 성공");
-          history.push('/main')
-        } else {
-          alert(res.payload.message);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-
-
-
-
-  return (
-    <div className="content">
-      <form class = "loginContent" onSubmit={onSubmitHandler}>
-        <input className="input_id" type="id" placeholder="id" onChange={onIdHandler}/>
-        <br />
-
-        <input className="input_pwd" type="password" placeholder="password" onChange={onPasswordHandler}/>
-
-        <br />
-        <button className="btn" type="submit">Login</button>
-      </form>  
-        
-        <br />
-        <a>카카오톡으로 로그인하기</a>
-        <br />
-        <a target="_blank" id="pw-inquiry" href="#">
-        <Link to="signup">회원가입</Link> | <Link to="findpw">비밀번호 찾기</Link>
-        </a>
-      
-    </div>
-  );
 }
 
-
-export default withRouter(LoginContent);
+export default LoginContent;
